5b59a87c928a609301e20854619005fd88ea9337
R1ch@rd <103750454+Richardesan@users.noreply.github.com>
2024-07-13 15:35:32 +0100

    update to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -19,7 +20,10 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          <Link to="/login">
+            Login button
+          </Link>
+         
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
